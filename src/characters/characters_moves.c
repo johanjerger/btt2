a714e39fd2ec8920350f3cb6dc34422c7ebd0d72
johanjerger <ocamposjuanc@gmail.com>
2017-08-31 13:58:14 -0300

    Adding the models of the characters moves

---
+++ src/characters/characters_moves.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "includes/characters.h"
+
+/*
+ *  This modules defines the game's characters
+ *  moves, everyone moves with a different strategy.
+ */
+
+int move_drugo(character * drugo)
+{
+  drugo->posX = 4;
+  return 8;
+}
+
+
+int move_pterodactyl(character * pterodactyl)
+{
+  return 0;
+}
