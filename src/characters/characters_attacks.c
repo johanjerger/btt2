e53cde227ac83c2db8c130e1487b8a06660fc76b
johanjerger <ocamposjuanc@gmail.com>
2017-08-31 13:58:40 -0300

    Addign the models of the characters attacks

---
+++ src/characters/characters_attacks.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "includes/characters.h"
+
+/*
+ *  This modules defines the game's characters
+ *  attacks, everyone attacks with a different strategy.
+ */
+
+int attack_drugo(character * drugo)
+{
+  drugo->posX = 4;
+  return 9;
+}
+
+
+int attack_pterodactyl(character * pterodactyl)
+{
+  return 0;
+}
