164258daf96fd2fbda78872b3d284aab415f9296
johanjerger <ocamposjuanc@gmail.com>
2017-09-01 17:17:48 -0300

    option shows methods, uses it later in the menu

---
+++ src/menu/option/options_shows.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "includes/options.h"
+
+/*
+ *  This modules defines the game's options
+ *  shows.
+ */
+
+int show_option(option * opt)
+{
+  printf("%s\n", opt->text);
+}
+
+
+int show_option_selected(option * opt)
+{
+  printf("%s\n", opt->text);
+}
