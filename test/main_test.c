553bb9a219711d076501c05a9a111ac750716df2
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-02 19:01:42 -0300

    Main controller for the test

---
+++ test/main_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "includes/characters_test.h"
+#include "../src/utilities/includes/colors.h"
+#include "../src/utilities/includes/utilities.h"
+
+int main()
+{
+
+        printf(GREEN "\n\t ... STARTING TESTS ... \n" RESET);
+
+        characters_test();
+
+        return 0;
+
+}
