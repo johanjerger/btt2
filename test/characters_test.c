f1aaf1fa8e5b1abff20bee3faa5e08467559da58
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-04 10:51:51 -0300

    changes with the new assert function

--- test/characters_test.c
+++ test/characters_test.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "includes/test.h"
 #include "../src/characters/includes/characters.h"
 #include "../src/characters/includes/characters_generator.h"
 #include "../src/characters/includes/characters_attacks.h"
 #include "../src/characters/includes/characters_moves.h"
-#include "../src/utilities/includes/colors.h"
-#include "../src/utilities/includes/utilities.h"
 
 int characters_test()
 {
@@ -13,43 +12,19 @@ int characters_test()
         character new_drugo = drugo(posX, posY);
         character new_pterodactyl = pterodactyl(posX, posY);
 
-        printf(GREEN "\n BEGINING Characters.c TEST\n\n" RESET);
+        make_test_title("characters");
 
-        sleep(500);
+        assert(new_drugo.body == '#' && new_pterodactyl.body == '<', "body");
 
-        (new_drugo.body == '#' && new_pterodactyl.body == '<') ?
-        printf(GREEN "\t - Body method    .................................... SUCCESS \n" RESET) :
-        fprintf(stderr, RED "\t - Body method    .................................... FAIL \n" RESET);
+        assert(new_drugo.posX == posX && new_pterodactyl.posX == posX, "posX");
 
-        sleep(500);
+        assert(new_drugo.posY == posY && new_pterodactyl.posY == posY, "posY");
 
-        (new_drugo.posX == posX && new_pterodactyl.posX == posX) ?
-        printf(GREEN "\t - posX method    .................................... SUCCESS \n" RESET) :
-        fprintf(stderr, RED "\t - posX method    .................................... FAIL \n" RESET);
+        assert(new_drugo.move(&new_drugo) == move_drugo(&new_drugo), "move");
 
-        sleep(500);
+        assert(new_drugo.attack(&new_drugo) == attack_drugo(&new_drugo), "attack");
 
-        (new_drugo.posY == posY && new_pterodactyl.posY == posY) ?
-        printf(GREEN "\t - posY method    .................................... SUCCESS \n" RESET) :
-        fprintf(stderr, RED "\t - posY method    .................................... FAIL \n" RESET);
-
-        sleep(500);
-
-        (new_drugo.move(&new_drugo) == move_drugo(&new_drugo) &&
-         new_pterodactyl.move(&new_pterodactyl) == move_pterodactyl(&new_pterodactyl)) ?
-        printf(GREEN "\t - Moves method   .................................... SUCCESS \n" RESET) :
-        fprintf(stderr, RED "\t - Moves method   ..................................... FAIL \n" RESET);
-
-        sleep(500);
-
-        (new_drugo.attack(&new_drugo) == attack_drugo(&new_drugo) &&
-         new_pterodactyl.attack(&new_pterodactyl) == attack_pterodactyl(&new_pterodactyl)) ?
-        printf(GREEN "\t - Attacks method .................................... SUCCESS \n" RESET) :
-        fprintf(stderr, RED "\t - Attacks method ....................................... FAIL \n" RESET);
-
-        sleep(500);
-
-        printf(GREEN "\n Characters.c TEST FINISH\n\n" RESET);
+        end_test_title("characters");
 
         return 0;
 }
