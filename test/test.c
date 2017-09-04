b910bed5a513dc7cd4c183feddc7f668587cc987
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-04 17:33:51 -0300

    Making some changes to the test funtions

--- test/test.c
+++ test/test.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "../src/utilities/includes/colors.h"
 #include "../src/utilities/includes/utilities.h"
 
 int make_test_title(char * test_name)
 {
+        // Method that make the Test title header
+
         printf(GREEN "\n BEGINING %s TEST\n\n" RESET, test_name);
         sleep(500);
 
@@ -16,9 +19,15 @@ int assert(bool success, char * method)
 {
         // assert method to avoid the duplicate code in tests...
 
+        // the spaces variable is only for output purpose
+        char spaces[] = "          ";
+        int size_of_spaces = strlen(spaces) - strlen(method);
+        spaces[size_of_spaces] = '\0';
+
+        // printing the test result.
         (success) ?
-        printf(GREEN "\t - %s method    .................................... SUCCESS \n" RESET, method) :
-        fprintf(stderr, RED "\t - %s method    .................................... FAIL \n" RESET, method);
+        printf(GREEN "\t - %s method %s ..............................     SUCCESS \n" RESET, method, spaces) :
+        fprintf(stderr, RED "\t - %s method %s ..............................     FAIL \n" RESET, method, spaces);
         sleep(500);
 
         return 0;
@@ -26,6 +35,8 @@ int assert(bool success, char * method)
 
 int end_test_title(char * test_name)
 {
+        // Method that announces the finish of the current Test
+
         printf(GREEN "\n %s TEST FINISH\n\n" RESET, test_name);
 
         return 0;
