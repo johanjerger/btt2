a9fee3094752cf4553b5eef7fc0b7dfa251c3686
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-05 16:26:20 -0300

    Rename of the option component

--- src/option/includes/options_generator.h
+++
@@ -1,8 +0,0 @@
-#ifndef BTT2_OPG_H
-#define BTT2_OPG_H
-
-  option unselected_option(char * text, int (*active_option)());
-
-  option selected_option(char * text, int (*active_option)());
-
-#endif
--- src/option/includes/options_shows.h
+++
@@ -1,8 +0,0 @@
-#ifndef BTT2_OPS_H
-#define BTT2_OPS_H
-
-  void show_option_unselected(option *);
-
-  void show_option_selected(option *);
-
-#endif
