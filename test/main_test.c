06a8496d9ad6ee25b4ad583758ae799885068efb
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-05 13:19:33 -0300

    Refactoring, adding the options test call

--- test/main_test.c
+++ test/main_test.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "includes/test.h"
 #include "includes/characters_test.h"
+#include "includes/option_test.h"
 #include "../src/utilities/includes/colors.h"
 #include "../src/utilities/includes/utilities.h"
 
 int main()
 {
-        printf(GREEN "\n\t ... STARTING TESTS ... \n" RESET);
+
+        make_header();
 
         characters_test();
+
+        option_test();
+
+        make_footer();
 
         return 0;
 }
