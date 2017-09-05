182d7c04622ca8adec0275211455e60d5d8eb6d1
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-05 16:23:29 -0300

    Renamed directory

---
+++ src/menus/includes/menus.h
@@ -0,0 +1,19 @@
+#ifndef BTT2_MN_H
+#define BTT2_MN_H
+
+/*
+ *  Struct that contains the attributes of the menues
+ *  it contains the menues representation, the options
+ *  and the uses functions.
+ */
+
+ #include "../../options_list/includes/options_list.h"
+
+typedef struct struct _menu
+{
+  char * title;
+  options_list * options;
+  int(*show_menu)(struct _menu *);
+} menu;
+
+#endif
