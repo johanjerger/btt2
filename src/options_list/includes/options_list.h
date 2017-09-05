f1b44f135f9cf6bc4c22b8dcc310521b73c9556c
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-05 16:24:09 -0300

    options list include

---
+++ src/options_list/includes/options_list.h
@@ -0,0 +1,15 @@
+#ifndef BTT2_OLL_H
+#define BTT2_OLL_H
+
+#include "../../options/includes/options.h"
+
+typedef struct _options_list
+{
+  option * option;
+  struct _options_list * next_option;
+  struct _options_list * previous_option;
+} options_list;
+
+options_list new_options_list(option *, int);
+
+#endif
