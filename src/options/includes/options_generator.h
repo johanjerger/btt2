8730730a62f0bba5abdbc2f7894671f275dea6f9
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-27 13:18:03 -0300

    Redefined the options generator parameter

--- src/options/includes/options_generator.h
+++ src/options/includes/options_generator.h
@@ -1,8 +1,8 @@
 #ifndef BTT2_OPG_H
 #define BTT2_OPG_H
 
-  option unselected_option(char * text, int (*active_option)());
+  option * unselected_option(char * text, int (*active_option)());
 
-  option selected_option(char * text, int (*active_option)());
+  option * selected_option(char * text, int (*active_option)());
 
 #endif
