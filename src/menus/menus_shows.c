27d955689a930c4f4928639d336b59dae771f3e3
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-27 13:21:56 -0300

    Added and debuged menus shows

---
+++ src/menus/menus_shows.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "includes/menus.h"
+#include "../options/includes/options.h"
+#include "../options/includes/options_generator.h"
+#include "../options_list/includes/options_list.h"
+
+int show_main_menu(menu * main_menu)
+{
+        options_list head = *(main_menu->options);
+        options_list * first_option = main_menu->options;
+
+        while(head.next_option != first_option) {
+                (head.option)->show_option(head.option);
+                head = *(head.next_option);
+        }
+        
+        (head.option)->show_option(head.option);
+
+        return 0;
+}
