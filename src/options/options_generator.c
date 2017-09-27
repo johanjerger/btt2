4c224bced93fe5eb0b150ad57385c85889a648a4
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-27 13:18:30 -0300

    Redefined the options generator function, more pointers

--- src/options/options_generator.c
+++ src/options/options_generator.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "includes/options.h"
 #include "includes/options_shows.h"
 
-option generate_option (char * text, void (*show_option)(option *), int (*active_option)(option *))
+option * generate_option (char * text, void (*show_option)(option *), int (*active_option)(option *))
 {
         //  Abtract generate method. Never used directly.
 
-        option new_option;
+        option * new_option = malloc(sizeof(option));
 
-        new_option.text = text;
-        new_option.show_option = show_option;
-        new_option.active_option = active_option;
+        new_option->text = (char *) malloc(strlen(text) + 1);
+        strcpy(new_option->text, text);
+        new_option->show_option = show_option;
+        new_option->active_option = active_option;
 
         return new_option;
 }
 
-option unselected_option(char * text, int (*active_option)())
+option * unselected_option(char * text, int (*active_option)())
 {
         return generate_option(text, show_option_unselected, active_option);
 }
 
-option selected_option(char * text, int (*active_option)())
+option * selected_option(char * text, int (*active_option)())
 {
         return generate_option(text, show_option_selected, active_option);
 }
