79bf5985d8fe17551fbc7b4efce709d30db318d8
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-27 15:00:20 -0300

    Title Added

--- src/menus/menus_shows.c
+++ src/menus/menus_shows.c
@@ -9,11 +9,13 @@ int show_main_menu(menu * main_menu)
         options_list head = *(main_menu->options);
         options_list * first_option = main_menu->options;
 
+        printf("%s\n", main_menu->title);
+
         while(head.next_option != first_option) {
                 (head.option)->show_option(head.option);
                 head = *(head.next_option);
         }
-        
+
         (head.option)->show_option(head.option);
 
         return 0;
