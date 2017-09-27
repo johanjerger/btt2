c95531eda599dfc1b5d34f6d540ab285ffcea147
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-27 15:00:33 -0300

    Changed the characters test

--- test/characters_test.c
+++ test/characters_test.c
@@ -9,20 +9,20 @@
 int characters_test()
 {
         int posX = 2, posY = 5;
-        character new_drugo = drugo(posX, posY);
-        character new_pterodactyl = pterodactyl(posX, posY);
+        character * new_drugo = drugo(posX, posY);
+        character * new_pterodactyl = pterodactyl(posX, posY);
 
         make_test_title("characters");
 
-        assert(new_drugo.body == '#' && new_pterodactyl.body == '<', "body");
+        assert(new_drugo->body == '#' && new_pterodactyl->body == '<', "body");
 
-        assert(new_drugo.posX == posX && new_pterodactyl.posX == posX, "posX");
+        assert(new_drugo->posX == posX && new_pterodactyl->posX == posX, "posX");
 
-        assert(new_drugo.posY == posY && new_pterodactyl.posY == posY, "posY");
+        assert(new_drugo->posY == posY && new_pterodactyl->posY == posY, "posY");
 
-        assert(new_drugo.move(&new_drugo) == move_drugo(&new_drugo), "move");
+        assert(new_drugo->move(new_drugo) == move_drugo(new_drugo), "move");
 
-        assert(new_drugo.attack(&new_drugo) == attack_drugo(&new_drugo), "attack");
+        assert(new_drugo->attack(new_drugo) == attack_drugo(new_drugo), "attack");
 
         end_test_title("characters");
 
