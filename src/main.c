bbabc693b48bac474782b6d693a239a9578f0aa9
johanjerger <ocamposjuancruz23@gmail.com>
2017-09-27 14:59:55 -0300

    Making the first steps

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "menus/includes/menus.h"
+#include "menus/includes/menus_generator.h"
 
 int main()
 {
+        menu * main_menu = new_main_menu();
 
+        main_menu->show_menu(main_menu);
+        
         return 0;
 }
