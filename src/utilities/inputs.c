e9a2a3ef3cdf7e3e2f89b095611793d7807a22d7
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-05 17:04:27 -0300

    Desacoplated methods

---
+++ src/utilities/inputs.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include "includes/utilities.h"
+
+
+char getch(void)
+{
+        struct termios oldattr, newattr;
+        short ch;
+        tcgetattr(1, &oldattr);
+        newattr = oldattr;
+        newattr.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(1, TCSANOW, &newattr);
+        ch = getchar();
+        tcsetattr(1, TCSANOW, &oldattr);
+        return ch;
+}
+
+
+char kbhit()
+{
+        struct termios oldt, newt;
+        char ch[20];
+        short oldf;
+
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+        if(!(fscanf(stdin,"%s", ch))) {
+                fprintf(stderr, "error -> %d\n", EIO);
+                exit(1);
+        }
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+        return ch[0];
+}
