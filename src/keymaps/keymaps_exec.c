08c55d2e2c06148fc93964585cf9b64b66676707
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-05 14:28:24 -0300

    Code beautified

--- src/keymaps/keymaps_exec.c
+++ src/keymaps/keymaps_exec.c
@@ -3,12 +3,12 @@
 
 int execute_keymap_action(menu * actual_menu, keymap * actual_keymap, char key)
 {
-  keymap * head = actual_keymap;
+        keymap * head = actual_keymap;
 
-  while(head->key != key) {
-    head = head->next_key;
-    if (head == actual_keymap) return 0 ;
-  }
+        while(head->key != key) {
+                head = head->next_key;
+                if (head == actual_keymap) return 0;
+        }
 
-  return head->action(actual_menu);
+        return head->action(actual_menu);
 }
