6800dfe0bdde5987ab26d28f7f010532591de5c4
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-05 13:56:28 -0300

    kbhit added

--- src/utilities/utilities.c
+++ src/utilities/utilities.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include "includes/utilities.h"
 #include "includes/colors.h"
 
 
@@ -14,7 +19,7 @@ int clear()
 }
 
 
-int sleep(int time)
+int btt_sleep(int time)
 {
         /*
            timespec struct is use for
@@ -31,4 +36,32 @@ int sleep(int time)
         }
 
         return 0;
+}
+
+char kbhit()
+{
+  struct termios oldt, newt;
+  char ch[20];
+  short oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  if(!(scanf("%s", ch))){
+    fprintf(stderr, "error -> %d\n", EIO);
+    exit(1);
+  }
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch[0] != EOF){
+    return ch[0];
+  }
+
+  return 0;
 }
