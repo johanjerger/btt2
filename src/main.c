cdfcf49bc5ae711931154410996d303c9210161f
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-05 16:12:44 -0300

    Fixed issue

--- src/main.c
+++ src/main.c
@@ -3,25 +3,21 @@
 #include "utilities/includes/utilities.h"
 #include "menus/includes/menus.h"
 #include "keymaps/includes/keymaps.h"
-#include "menus/includes/menus_generator.h"
 
 int main()
 {
         menu * main_menu = new_main_menu();
+        keymap * main_menu_keymap = new_main_menu_keymap();
 
         clear();
         main_menu->show_menu(main_menu);
 
-        keymap * main_menu_keymap = new_main_menu_keymap();
-
         while(1) {
-
                 char key;
 
-                key = kbhit();
-
+                key = getch();
                 main_menu_keymap->exec(main_menu, main_menu_keymap, key);
-
+                clear();
                 main_menu->show_menu(main_menu);
 
         }
