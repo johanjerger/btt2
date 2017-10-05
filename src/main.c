ae63121a496bd324206b5e48cd531b1cb7c81b3d
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-05 17:02:05 -0300

    Refactoring

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "utilities/includes/utilities.h"
 #include "menus/includes/menus.h"
 #include "keymaps/includes/keymaps.h"
+#include "utilities/includes/inputs.h"
 
 int main()
 {
         menu * main_menu = new_main_menu();
         keymap * main_menu_keymap = new_main_menu_keymap();
 
-        clear();
         main_menu->show_menu(main_menu);
 
         while(1) {
@@ -17,9 +16,7 @@ int main()
 
                 key = getch();
                 main_menu_keymap->exec(main_menu, main_menu_keymap, key);
-                clear();
                 main_menu->show_menu(main_menu);
-
         }
 
         return 0;
