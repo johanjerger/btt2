2918c75348437bbef21c99d5e073e28c6ef0f4b3
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-05 17:02:18 -0300

    Refactoring

--- src/menus/menus_shows.c
+++ src/menus/menus_shows.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "includes/menus.h"
+#include "../utilities/includes/utilities.h"
 #include "../options/includes/options.h"
 #include "../options_list/includes/options_list.h"
 
@@ -8,6 +9,7 @@ int show_main_menu(menu * main_menu)
         options_list head = *(main_menu->options);
         options_list * first_option = main_menu->options;
 
+        clear();
         printf("%s\n", main_menu->title);
 
         while(head.next_option != first_option) {
