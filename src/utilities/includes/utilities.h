2aafe0a6c3eb942742bd77e8d1c3ec52c0c980dd
johanjerger <ocamposjuancruz23@gmail.com>
2017-10-05 17:03:32 -0300

    Deacoplate methods

--- src/utilities/includes/utilities.h
+++ src/utilities/includes/utilities.h
@@ -1,18 +1,10 @@
 #ifndef BTT2_UTL_H
 #define BTT2_UTL_H
 
-#define BUFFER_SIZE 256
-
-#include "../../menus/includes/menus.h"
-
   int clear();
 
   int btt_sleep(int);
 
-  char getch();
-
-  char kbhit();
-
-  int go_out(menu *);
+  int go_out();
 
 #endif
