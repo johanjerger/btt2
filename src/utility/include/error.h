93afe4f2239cca5d68e1b7da7a0112012952de52
johan <johan@kbp1-V01-F55873.synapse.com>
2017-12-19 07:19:37 -0800

    errors handlers performed

--- src/utility/include/error.h
+++ src/utility/include/error.h
@@ -10,5 +10,7 @@
 #define MALLOC_ERROR_MSG_MENU "error while allocting menu_t type pointer."
 #define MALLOC_ERROR_MSG_OPTION "error while allocting option_t type pointer."
 #define MALLOC_ERROR_MSG_OPTION_LIST "error while allocting option_list_t type pointer."
+#define CLEAR_ERROR_MSG "error calling clear function"
+#define SLEEP_ERROR_MSG "error calling sleep function"
 
 #endif
--- src/utility/utility.c
+++ src/utility/utility.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "include/error.h"
 #include "include/utility.h"
 #include "include/color.h"
 
 
 void check_error(void * value, void * error, int error_code, char * error_msg)
 {
-  if(value == error) {
-    fprintf(stderr, RED "error -> %d\nmsg  -> %s\n" RESET,
-            error_code, error_msg);
-    exit(error_code);
-  }
+        if(value == error) {
+                fprintf(stderr, RED "\nerror -> %d\nmsg  -> %s\n" RESET,
+                        error_code, error_msg);
+                exit(error_code);
+        }
 }
 
 void clear()
 {
         btt_sleep(25);
         if((system("clear")) == -1) {
-                fprintf(stderr, RED "Error calling system \"clear\" function\n" RESET);
+                fprintf(stderr, RED "\nerror -> %d\nmsg  -> %s\n" RESET,
+                        CLEAR_ERROR, CLEAR_ERROR_MSG);
+                exit(CLEAR_ERROR);
         }
 }
 
@@ -30,12 +33,15 @@ void btt_sleep(int time)
         ts.tv_nsec = (time % 1000) * 1000000;
 
         if((nanosleep(&ts, NULL)) == -1) {
-                fprintf(stderr, RED "Error calling stdlib \"nanosleep\" function\n" RESET);
+                fprintf(stderr, RED "\nerror -> %d\nmsg  -> %s\n" RESET,
+                        SLEEP_ERROR, SLEEP_ERROR_MSG);
+                exit(SLEEP_ERROR);
         }
 }
 
 
 void go_out()
 {
+        clear();
         exit(0);
 }
