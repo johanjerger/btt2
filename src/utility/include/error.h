d32223cf8e7b1a88cc5f80bcd81d29d45deff764
johanjerger <ocamposjuancruz23@gmail.com>
2017-12-21 12:23:14 -0300

    Added char * allocation error msg

--- src/utility/include/error.h
+++ src/utility/include/error.h
@@ -8,6 +8,7 @@
 #define MALLOC_ERROR_MSG_CHARACTER "error while allocting character_t type pointer."
 #define MALLOC_ERROR_MSG_KEYMAP "error while allocting keymap_t type pointer."
 #define MALLOC_ERROR_MSG_MENU "error while allocting menu_t type pointer."
+#define MALLOC_ERROR_MSG_MENU_TITLE "error while allocting char * type pointer in menu.c."
 #define MALLOC_ERROR_MSG_OPTION "error while allocting option_t type pointer."
 #define MALLOC_ERROR_MSG_OPTION_LIST "error while allocting option_list_t type pointer."
 #define CLEAR_ERROR_MSG "error calling clear function"
