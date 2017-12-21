368d6f851f11af0a6c836836a944a4746418acd9
johanjerger <ocamposjuancruz23@gmail.com>
2017-12-21 12:24:38 -0300

    Added compatibility macros

--- src/utility/utility.c
+++ src/utility/utility.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef __unix__
 #include <time.h>
+#endif
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include "include/error.h"
 #include "include/utility.h"
 #include "include/color.h"
@@ -18,7 +23,12 @@ void check_error(void * value, void * error, int error_code, char * error_msg)
 void clear()
 {
         btt_sleep(25);
+#ifdef _WIN32
+        if((system("cls")) == -1) {
+#endif
+#ifdef __unix__
         if((system("clear")) == -1) {
+#endif
                 fprintf(stderr, RED "\nerror -> %d\nmsg  -> %s\n" RESET,
                         CLEAR_ERROR, CLEAR_ERROR_MSG);
                 exit(CLEAR_ERROR);
@@ -28,6 +38,10 @@ void clear()
 
 void btt_sleep(int time)
 {
+  #ifdef _WIN32
+    Sleep(time);
+  #endif
+  #ifdef __unix__
         struct timespec ts;
         ts.tv_sec = time / 1000;
         ts.tv_nsec = (time % 1000) * 1000000;
@@ -37,6 +51,7 @@ void btt_sleep(int time)
                         SLEEP_ERROR, SLEEP_ERROR_MSG);
                 exit(SLEEP_ERROR);
         }
+  #endif
 }
 
 
