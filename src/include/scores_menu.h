c83a0fa65f739f327df4dd015c8d6c307811fdb0
johanjerger <ocamposjuancruz23@gmail.com>
2017-12-22 14:10:29 -0300

    Added scores menu files

---
+++ src/include/scores_menu.h
@@ -0,0 +1,6 @@
+#ifndef BTT2_SCT_MENU_H
+#define BTT2_SCT_MENU_H
+
+  void scores_menu();
+
+#endif
---
+++ src/scores_menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "menu/include/menu.h"
+#include "keymap/include/keymap.h"
+#include "utility/include/input.h"
+#include "score_table/include/score_table.h"
+
+void scores_menu()
+{
+        menu_t * scores_menu = new_scores_menu();
+        keymap_t * scores_keymap = new_scores_keymap();
+        scores_menu->show(scores_menu);
+
+        while(true) {
+                char key;
+                key = btt_getch();
+
+                scores_keymap->exec(scores_menu, scores_keymap, key);
+                scores_menu->show(scores_menu);
+        }
+}
