ec7e1f817ec50040a0c6eb3e0d3d1379f52d1720
johanjerger <ocamposjuancruz23@gmail.com>
2017-12-26 17:13:55 -0300

    Avoided if statement

--- src/keymap/execute.c
+++ src/keymap/execute.c
@@ -1,13 +1,14 @@
 #include <string.h>
 #include "include/keymap.h"
 
-void execute_keymap_action(menu_t * actual_menu, keymap_t * actual_keymap, char key)
+void execute_keymap_action(menu_t * actual_menu, keymap_t * actual_keymap,
+                           char key)
 {
         keymap_t * head = actual_keymap;
 
         while(head->key != key && head != head->next_key) {
                 head = head->next_key;
         }
-        if (head->key != key) return;
+
         head->action(actual_menu);
 }
