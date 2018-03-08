bfaa5b4675e5cb9b96eea7658981f8f193121c4c
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 11:13:28 -0300

    Deleted useless macros and refactored

--- src/score_table/open.c
+++ src/score_table/open.c
@@ -5,20 +5,25 @@
 #include "../utility/include/error.h"
 #include "include/score_table.h"
 
-FILE * score_table_open(score_table_t * score_table)
+FILE *
+score_table_open (score_table_t * score_table)
 {
-        FILE * score_table_file;
-        score_table_file = fopen(score_table->file_name, "r");
-        if(score_table_file == NULL) {
-                score_table_file = fopen(score_table->file_name, "w");
-                check_error(score_table_file, NULL, CREATE_ERROR,
-                            CREATE_ERROR_MSG_SCORE_TABLE);
-                for (int i = 0; i < SCORE_TABLE_SIZE; ++i) {
-                        score_t * score = new_score("GateLlman Job", 0, 1);
-                        score->write(score_table_file, score);
-                }
-                fclose(score_table_file);
-                score_table_file = fopen(score_table->file_name, "r");
-        }
-        return score_table_file;
+		FILE * score_table_file;
+		score_table_file = fopen(score_table->file_name, "r");
+
+		if(score_table_file == NULL)
+		{
+				score_table_file = fopen(score_table->file_name, "w");
+				check_error(score_table_file, NULL, CREATE_ERROR,
+				            CREATE_ERROR_MSG_SCORE_TABLE);
+				for (int i = 0; i < score_table->size; i++)
+				{
+						score_t * score = new_score("GateLlman Job", 0, 1);
+						score->write(score_table_file, score);
+				}
+				fclose(score_table_file);
+				score_table_file = fopen(score_table->file_name, "r");
+		}
+
+		return score_table_file;
 }
