8f98a17c30c5ea9c767fce52d829285d66ca9a39
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 16:46:32 -0300

    New art for the main banner

---
+++ ascii_art/art2.h
@@ -0,0 +1,20 @@
+#ifndef BTT_ART_H
+#define BTT_ART_H
+
+#include "../src/utility/include/color.h"
+
+#define MAIN_ART \
+        "\n\t\t" BRED "                                              " RESET "\n" \
+        "\t\t" BRED "         " BBLACK "       " BRED "      " BBLACK "       " BRED "  " BBLACK "       " BRED "        " RESET "\n" \
+        "\t\t" BRED "         " BBLACK " " BRED "      " BBLACK " " BRED "        " BBLACK " " BRED "        " BBLACK " " BRED "           " RESET "\n" \
+        "\t\t" BRED "         " BBLACK " " BRED "       " BBLACK " " BRED "       " BBLACK " " BRED "        " BBLACK " " BRED "           " RESET "\n" \
+        "\t\t" BRED "         " BBLACK "         " BRED "       " BBLACK " " BRED "        " BBLACK " " BRED "           " RESET "\n" \
+        "\t\t" BRED "         " BBLACK " " BRED "        " BBLACK " " BRED "      " BBLACK " " BRED "        " BBLACK " " BRED "           " RESET "\n" \
+        "\t\t" BRED "         " BBLACK " " BRED "         " BBLACK " " BRED "     " BBLACK " " BRED "        " BBLACK " " BRED "           " RESET "\n" \
+        "\t\t" BRED "         " BBLACK " " BRED "         " BBLACK " " BRED "     " BBLACK " " BRED "        " BBLACK " " BRED "           " RESET "\n" \
+        "\t\t" BRED "         " BBLACK "          " BRED "       " BBLACK " " BRED "        " BBLACK " " BRED "          " RESET "\n" \
+        "\t\t" BRED "                                              " RESET "\n" \
+        "\t\t" BRED BLACK "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾" RESET "\n"
+
+
+#endif
