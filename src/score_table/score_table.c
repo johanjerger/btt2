700bf86d17c546430bbb4c42ce8062f3c9678698
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 11:13:41 -0300

    Added size variable

--- src/score_table/score_table.c
+++ src/score_table/score_table.c
@@ -9,26 +9,34 @@
 #include "include/show.h"
 #include "include/open.h"
 
-score_table_t * new_score_table(char * title)
+score_table_t *
+new_score_table (char * title)
 {
-        score_table_t * new_score_table;
-        char * path = root_path;
+		score_table_t * new_score_table;
+		char * path = scores_path;
 
-        new_score_table = (score_table_t *) malloc(sizeof(score_table_t));
-        check_error(new_score_table, NULL, MALLOC_ERROR, MALLOC_ERROR_MSG_SCORE_TABLE);
+		new_score_table = (score_table_t *) malloc(sizeof(score_table_t));
+		check_error(new_score_table, NULL, MALLOC_ERROR,
+		            MALLOC_ERROR_MSG_SCORE_TABLE);
 
-        new_score_table->file_name = (char *) malloc((strlen(title) + strlen(path)) * sizeof(char) + 1);
-        check_error(new_score_table->file_name, NULL, MALLOC_ERROR, MALLOC_ERROR_MSG_SCORE_TABLE_FILE);
-        strcpy(new_score_table->file_name, path);
-        strcat(new_score_table->file_name, title);
+		int to_alloc = strlen(title) + strlen(path);
+		new_score_table->file_name = (char *) malloc(to_alloc * sizeof(char) + 1);
+		check_error(new_score_table->file_name, NULL, MALLOC_ERROR,
+		            MALLOC_ERROR_MSG_SCORE_TABLE_FILE);
 
-        new_score_table->title = (char *) malloc(strlen(title) * sizeof(char) + 1);
-        check_error(new_score_table->title, NULL, MALLOC_ERROR, MALLOC_ERROR_MSG_SCORE_TABLE_TITLE);
-        strcpy(new_score_table->title, title);
+		strcpy(new_score_table->file_name, path);
+		strcat(new_score_table->file_name, title);
 
-        new_score_table->add = score_table_add;
-        new_score_table->show = score_table_show;
-        new_score_table->open = score_table_open;
+		new_score_table->title = (char *) malloc(strlen(title) * sizeof(char) + 1);
+		check_error(new_score_table->title, NULL, MALLOC_ERROR,
+		            MALLOC_ERROR_MSG_SCORE_TABLE_TITLE);
 
-        return new_score_table;
+		strcpy(new_score_table->title, title);
+
+		new_score_table->size = SCORE_TABLE_SIZE;
+		new_score_table->add = score_table_add;
+		new_score_table->show = score_table_show;
+		new_score_table->open = score_table_open;
+
+		return new_score_table;
 }
