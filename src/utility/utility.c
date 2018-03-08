a49ab0f156474f29e9aac2700ec14662f7d64501
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 10:33:00 -0300

    Indentation changed

--- src/utility/utility.c
+++ src/utility/utility.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #ifdef _WIN32
-        #include <windows.h>
+#include <windows.h>
 #elif __unix__
-        #include <time.h>
-        #include <sys/stat.h>
+#include <time.h>
+#include <sys/stat.h>
 #endif
 #include <errno.h>
 #include "../include/btt2.h"
@@ -15,29 +15,31 @@
 
 void check_error(void * value, void * error, int error_code, char * error_msg)
 {
-        if(value == error) {
-                fprintf(stderr, RED "\nerror -> %d\nmsg   -> %s\n" RESET,
-                        error_code, error_msg);
-                exit(error_code);
-        }
+		if(value == error)
+		{
+				fprintf(stderr, RED "\nerror -> %d\nmsg   -> %s\n" RESET,
+				        error_code, error_msg);
+				exit(error_code);
+		}
 }
 
 
 void check_int_error(int value, int error, int error_code, char * error_msg)
 {
-        if(value == error) {
-                fprintf(stderr, RED "\nerror -> %d\nmsg   -> %s\n" RESET,
-                        error_code, error_msg);
-                exit(error_code);
-        }
+		if(value == error)
+		{
+				fprintf(stderr, RED "\nerror -> %d\nmsg   -> %s\n" RESET,
+				        error_code, error_msg);
+				exit(error_code);
+		}
 }
 
 #ifdef _WIN32
 
 void create_directory(char * path)
 {
-        check_int_error(CreateDirectory(path, NULL), 0,
-                        ERROR_CREATING_DIRECTORY, ERROR_CREATING_DIRECTORY_MSG);
+		check_int_error(CreateDirectory(path, NULL), 0,
+		                ERROR_CREATING_DIRECTORY, ERROR_CREATING_DIRECTORY_MSG);
 }
 
 #elif __unix__
@@ -46,9 +48,9 @@ void create_directory(char * path)
 
 void create_directory(char * path)
 {
-        if(mkdir(path, 0777) != EEXIST)
-                if(mkdir(path, 0777) != EEXIST)
-                        fprintf(stderr, "%s\n", "error");
+		if(mkdir(path, 0777) != EEXIST)
+				if(mkdir(path, 0777) != EEXIST)
+						fprintf(stderr, "%s\n", "error");
 }
 
 #endif
@@ -62,16 +64,16 @@ void create_directory(char * path)
 #ifdef _WIN32
 void btt_clear()
 {
-        btt_sleep(25);
-        check_int_error(system("cls"), -1, CLEAR_ERROR, CLEAR_ERROR_MSG);
+		btt_sleep(25);
+		check_int_error(system("cls"), -1, CLEAR_ERROR, CLEAR_ERROR_MSG);
 }
 
 #elif __unix__
 
 void btt_clear()
 {
-        btt_sleep(25);
-        check_int_error(system("clear"), -1, CLEAR_ERROR, CLEAR_ERROR_MSG);
+		btt_sleep(25);
+		check_int_error(system("clear"), -1, CLEAR_ERROR, CLEAR_ERROR_MSG);
 }
 
 #endif
@@ -85,40 +87,40 @@ void btt_clear()
 #ifdef _WIN32
 void btt_sleep(int time)
 {
-        Sleep(time);
+		Sleep(time);
 }
 
 #elif __unix__
 
 void btt_sleep(int time)
 {
-        struct timespec ts;
-        ts.tv_sec = time / 1000;
-        ts.tv_nsec = (time % 1000) * 1000000;
+		struct timespec ts;
+		ts.tv_sec = time / 1000;
+		ts.tv_nsec = (time % 1000) * 1000000;
 
-        check_int_error(nanosleep(&ts, NULL), -1, SLEEP_ERROR, SLEEP_ERROR_MSG);
+		check_int_error(nanosleep(&ts, NULL), -1, SLEEP_ERROR, SLEEP_ERROR_MSG);
 }
 #endif
 
 void go_out()
 {
-        btt_clear();
-        game = false;
+		btt_clear();
+		game = false;
 }
 
 void go_out_scores()
 {
-        btt_clear();
-        scores = false;
+		btt_clear();
+		scores = false;
 }
 
 void go_out_difficult()
 {
-        btt_clear();
-        difficult = false;
+		btt_clear();
+		difficult = false;
 }
 
 void do_nothing()
 {
-        return;
+		return;
 }
