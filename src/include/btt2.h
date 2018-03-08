9de5cd3e7c8240878d4a2713a1c1fb732bb4cf09
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 11:11:54 -0300

    Code cleaned

--- src/include/btt2.h
+++ src/include/btt2.h
@@ -3,27 +3,25 @@
 
 #include <stdbool.h>
 
-// root path for games files
+// Defining the bars depending on the operating system
+#ifdef _WIN32
+#define BAR "\\"
+#elif __unix__
+#define BAR "/"
+#endif
 
+// root path for games files
 char * home_path;
 char * root_path;
 char * scores_path;
 char * config_path;
 
-#ifdef _WIN32
-  #define BAR "\\"
-#elif __unix__
-  #define BAR "/"
-#endif
-
 // global status variables
-
 bool game;
 bool scores;
 bool difficult;
 
 // global keys configurations
-
 char action_key;
 char up_key;
 char down_key;
