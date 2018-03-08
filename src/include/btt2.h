9789668eb77eccafa194ef3d15a0b1fdbd82cdf9
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 10:29:34 -0300

    Added the global variables for home, root, scores and config folders paths

--- src/include/btt2.h
+++ src/include/btt2.h
@@ -5,7 +5,10 @@
 
 // root path for games files
 
-  char * root_path;
+char * home_path;
+char * root_path;
+char * scores_path;
+char * config_path;
 
 #ifdef _WIN32
   #define BAR "\\"
@@ -15,17 +18,18 @@
 
 // global status variables
 
-  bool game;
-  bool scores;
-  bool difficult;
+bool game;
+bool scores;
+bool difficult;
 
 // global keys configurations
 
-  char action_key;
-  char up_key;
-  char down_key;
-  char left_key;
-  char right_key;
-  char item_key;
+char action_key;
+char up_key;
+char down_key;
+char left_key;
+char right_key;
+char item_key;
+char enter_key;
 
 #endif
