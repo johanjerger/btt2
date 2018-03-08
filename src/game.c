0537079caade76f3d6db876807dbae15c092a09e
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 12:56:43 -0300

    Added the game files

---
+++ src/game.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "utility/include/utility.h"
+
+void
+game (void)
+{
+		// TODO configurate the start of the game ...
+		printf("game start\n");
+		btt_sleep(2000);
+}
---
+++ src/include/game.h
@@ -0,0 +1,13 @@
+#ifndef BTT2_GAME_H
+#define BTT2_GAME_H
+
+// This file define game global variables
+int start_ammo;
+int maximum_ammo;
+int start_health;
+int maximum_health;
+int start_luck;
+
+void game (void);
+
+#endif
