e67ca708addef52ccc7f73b6a1da4d09e9224be8
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 11:12:28 -0300

    Deleted useless macros and refactored

--- src/score_table/add.c
+++ src/score_table/add.c
@@ -3,23 +3,22 @@
 #include "../score/include/score.h"
 #include "include/score_table.h"
 
-void score_table_add (score_table_t * score_table, score_t * new_score)
+void
+score_table_add (score_table_t * score_table, score_t * new_score)
 {
-        FILE * score_table_file = NULL;
-        score_t * scores[SCORE_TABLE_SIZE + 1];
+		FILE * score_table_file = NULL;
+		score_t * scores[score_table->size + 1];
 
-        score_table_file = score_table->open(score_table);
-
-        for (int i = 0; i < SCORE_TABLE_SIZE; i++) {
-                scores[i] = new_score->read(score_table_file);
-        }
-        scores[SCORE_TABLE_SIZE] = new_score;
-
-        qsort(scores, SCORE_TABLE_SIZE + 1, sizeof(score_t), new_score->compare);
-
-        for (int i = SCORE_TABLE_SIZE; i > 0; i--) {
-                new_score->write(score_table_file, scores[i]);
-        }
-
-        fclose(score_table_file);
+		score_table_file = score_table->open(score_table);
+		for (int i = 0; i < score_table->size; i++)
+		{
+				scores[i] = new_score->read(score_table_file);
+		}
+		scores[score_table->size] = new_score;
+		qsort(scores, score_table->size + 1, sizeof(score_t), new_score->compare);
+		for (int i = score_table->size; i != 0; i--)
+		{
+				new_score->write(score_table_file, scores[i]);
+		}
+		fclose(score_table_file);
 }
