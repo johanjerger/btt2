ad3c934261989f2c191dadcce226b18bb10ea4a4
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 10:30:09 -0300

    Identations changed

--- src/keymap/include/keymap.h
+++ src/keymap/include/keymap.h
@@ -3,21 +3,21 @@
 
 #include "../../menu/include/menu.h"
 
-  typedef struct _keymap
-  {
-    char key;
-    struct _keymap * next_key;
-    void (*action)();
-    void (*execute)(menu_t *, struct _keymap *, char);
-    void (*append)(struct _keymap *, char, void (*)());
-  } keymap_t;
+typedef struct _keymap
+{
+        char key;
+        struct _keymap * next_key;
+        void (*action)();
+        void (*execute)(menu_t *, struct _keymap *, char);
+        void (*append)(struct _keymap *, char, void (*)());
+} keymap_t;
 
-  keymap_t * new_keymap(char, void (*)());
+keymap_t * new_keymap(char, void (*)());
 
-  keymap_t * new_main_menu_keymap();
+keymap_t * new_main_menu_keymap();
 
-  keymap_t * new_difficult_menu_keymap();
+keymap_t * new_difficult_menu_keymap();
 
-  keymap_t * new_scores_keymap();
+keymap_t * new_scores_keymap();
 
 #endif
