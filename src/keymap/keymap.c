2e34e12ba92322add180dbd0563539fca16e1202
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 10:30:28 -0300

    Code modified to use the keys global variables

--- src/keymap/keymap.c
+++ src/keymap/keymap.c
@@ -3,57 +3,59 @@
 #include "include/keymap.h"
 #include "include/execute.h"
 #include "include/append.h"
+#include "../include/btt2.h"
 #include "../menu/include/menu.h"
 #include "../menu/include/change_option.h"
 #include "../menu/include/execute.h"
 #include "../utility/include/utility.h"
 #include "../utility/include/error.h"
 
-
-keymap_t * new_keymap(char key, void (*action)())
+keymap_t *
+new_keymap (char key, void (*action)())
 {
-        keymap_t * new_keymap;
+		keymap_t * new_keymap;
 
-        new_keymap = (keymap_t *) malloc(sizeof(keymap_t));
-        check_error(new_keymap, NULL, MALLOC_ERROR, MALLOC_ERROR_MSG_KEYMAP);
+		new_keymap = (keymap_t *) malloc(sizeof(keymap_t));
+		check_error(new_keymap, NULL, MALLOC_ERROR, MALLOC_ERROR_MSG_KEYMAP);
 
-        new_keymap->key = key;
-        new_keymap->action = action;
-        new_keymap->execute = execute_keymap_action;
-        new_keymap->next_key = new_keymap;
-        new_keymap->append = keymap_append;
+		new_keymap->key = key;
+		new_keymap->action = action;
+		new_keymap->execute = execute_keymap_action;
+		new_keymap->next_key = new_keymap;
+		new_keymap->append = keymap_append;
 
-        return new_keymap;
+		return new_keymap;
 }
 
-keymap_t * new_main_menu_keymap()
+keymap_t *
+new_main_menu_keymap (void)
 {
-        keymap_t * keymap = new_keymap('w', select_previous_option);
-        keymap->append(keymap, 's', select_next_option);
-        keymap->append(keymap, 'k', execute_menu);
-        keymap->append(keymap, (char) 777, do_nothing);
+		keymap_t * keymap = new_keymap(up_key, select_previous_option);
+		keymap->append(keymap, down_key, select_next_option);
+		keymap->append(keymap, enter_key, execute_menu);
+		keymap->append(keymap, (char) 777, do_nothing);
 
-        return keymap;
+		return keymap;
 }
 
-keymap_t * new_difficult_menu_keymap()
+keymap_t *
+new_difficult_menu_keymap (void)
 {
+		keymap_t * keymap = new_keymap(up_key, select_previous_option);
+		keymap->append(keymap, down_key, select_next_option);
+		keymap->append(keymap, enter_key, execute_menu);
+		keymap->append(keymap, (char) 777, do_nothing);
 
-        keymap_t * keymap = new_keymap('w', select_previous_option);
-        keymap->append(keymap, 's', select_next_option);
-        keymap->append(keymap, 'k', execute_menu);
-        keymap->append(keymap, (char) 777, do_nothing);
-
-        return keymap;
+		return keymap;
 }
 
-keymap_t * new_scores_keymap()
+keymap_t *
+new_scores_keymap (void)
 {
+		keymap_t * keymap = new_keymap(left_key, select_previous_option);
+		keymap->append(keymap, right_key, select_next_option);
+		keymap->append(keymap, enter_key, execute_menu);
+		keymap->append(keymap, (char) 777, do_nothing);
 
-        keymap_t * keymap = new_keymap('a', select_previous_option);
-        keymap->append(keymap, 'd', select_next_option);
-        keymap->append(keymap, 'k', execute_menu);
-        keymap->append(keymap, (char) 777, do_nothing);
-
-        return keymap;
+		return keymap;
 }
