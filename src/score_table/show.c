7613e70ae340bfeb2df06309c74c317541f4d27f
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 11:13:52 -0300

    Deleted useless macros and refactored

--- src/score_table/show.c
+++ src/score_table/show.c
@@ -3,14 +3,16 @@
 #include "../score/include/score.h"
 #include "include/score_table.h"
 
-void score_table_show (score_table_t * score_table)
+void
+score_table_show (score_table_t * score_table)
 {
-        FILE * score_table_file = NULL;
-        score_t * aux_score = new_score("aux", 0, 1);
-        score_table_file = score_table->open(score_table);
-        for (int i = 0; i < SCORE_TABLE_SIZE; i++) {
-                score_t * score = aux_score->read(score_table_file);
-                score->show(score);
-        }
-        fclose(score_table_file);
+		FILE * score_table_file = NULL;
+		score_t * aux_score = new_score("aux", 0, 1);
+		score_table_file = score_table->open(score_table);
+		for (int i = 0; i != score_table->size; i++)
+		{
+				score_t * score = aux_score->read(score_table_file);
+				score->show(score);
+		}
+		fclose(score_table_file);
 }
