430b747d5027c31577c62d1877a31ff4fb44e426
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 10:28:33 -0300

    Defining the global keys variables, this file needs to be refactored

--- src/configuration/keys.c
+++ src/configuration/keys.c
@@ -5,12 +5,26 @@
 
 void keys_configuration()
 {
-        char * keys_conf_path;
+		char * keys_conf_path;
 
-        keys_conf_path = (char *) malloc(strlen(root_path) * sizeof(char) + 20);
+		keys_conf_path = (char *) malloc(strlen(root_path) * sizeof(char) + 20);
 
-        strcpy(keys_conf_path, root_path);
-        strcat(keys_conf_path, "keys_conf");
+		strcpy(keys_conf_path, root_path);
+		strcat(keys_conf_path, "keys_conf");
 
-        printf("%s\n", keys_conf_path);
+		// This initializations must be written in a configurable file later
+
+		action_key = 'k';
+		up_key = 'w';
+		down_key = 's';
+		left_key = 'a';
+		right_key = 'd';
+		item_key = 'q';
+#ifdef _WIN32
+		enter_key = 13;
+#elif __unix__
+		enter_key = 10;
+#endif
+
+		printf("%s\n", keys_conf_path);
 }
