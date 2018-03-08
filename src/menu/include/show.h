ac486c3df74e522a8a7a4d996f7ff3610ad98c7a
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 11:50:28 -0300

    Deleted duplicated code

--- src/menu/include/show.h
+++ src/menu/include/show.h
@@ -1,8 +1,7 @@
 #ifndef BTT2_MNS_H
 #define BTT2_MNS_H
 
-  void show_main_menu(menu_t *);
-  void show_difficult(menu_t *);
-  void show_scores(menu_t *);
+void show_standard_menu (menu_t *);
+void show_scores (menu_t *);
 
 #endif
--- src/menu/show.c
+++ src/menu/show.c
@@ -6,44 +6,34 @@
 #include "../../ascii_art/art.h"
 #include "../score_table/include/score_table.h"
 
-void show_main_menu(menu_t * menu)
+void
+print_art (void)
 {
-        option_list_t * head = menu->options;
-
-        btt_clear();
-        printf(MAIN_ART);
-        printf("\n\t\t\t\t  %s\n", menu->title);
-
-        while(head->next != menu->options) {
-                (head->option)->show(head->option);
-                head = head->next;
-        }
-
-        (head->option)->show(head->option);
+		btt_clear();
+		printf(MAIN_ART);
 }
 
-void show_difficult(menu_t * menu)
+void
+show_standard_menu (menu_t * menu)
 {
-        option_list_t * head = menu->options;
-
-        btt_clear();
-        printf(MAIN_ART);
-        printf("\n\t\t\t\t  %s\n", menu->title);
-
-        while(head->next != menu->options) {
-                (head->option)->show(head->option);
-                head = head->next;
-        }
-
-        (head->option)->show(head->option);
+		print_art();
+		printf("\n\t\t\t\t  %s\n", menu->title);
+		option_list_t * head = menu->options;
+		while(head->next != menu->options)
+		{
+				(head->option)->show(head->option);
+				head = head->next;
+		}
+		(head->option)->show(head->option);
 }
 
-void show_scores(menu_t * menu)
+void
+show_scores (menu_t * menu)
 {
-        score_table_t * scores;
-
-        btt_clear();
-        printf("\n\t\t\t\t  %s %s\n", menu->selected_option->option->text, menu->title);
-        scores = new_score_table(menu->selected_option->option->text);
-        scores->show(scores);
+		print_art();
+		char * score_table_name = menu->selected_option->option->text;
+		printf("\n\t\t\t\t  %s %s\n", score_table_name, menu->title);
+		score_table_t * scores;
+		scores = new_score_table(score_table_name);
+		scores->show(scores);
 }
