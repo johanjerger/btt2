f5ad45fca923a15bf80f6fa9b85d29fc9cca859f
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-08 10:31:46 -0300

    Identations changed

--- src/utility/input.c
+++ src/utility/input.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #ifdef _WIN32
-        #include <conio.h>
+		#include <conio.h>
 #elif __unix__
-        #include <termios.h>
-        #include <unistd.h>
-        #include <errno.h>
-        #include <fcntl.h>
+		#include <termios.h>
+		#include <unistd.h>
+		#include <errno.h>
+		#include <fcntl.h>
 #endif
 #include "include/error.h"
 #include "include/utility.h"
@@ -21,22 +21,22 @@
 #ifdef _WIN32
 char btt_getch(void)
 {
-        return _getch();
+		return _getch();
 }
 
 #elif __unix__
 
 char btt_getch(void)
 {
-        struct termios oldattr, newattr;
-        short ch;
-        tcgetattr(1, &oldattr);
-        newattr = oldattr;
-        newattr.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(1, TCSANOW, &newattr);
-        ch = getchar();
-        tcsetattr(1, TCSANOW, &oldattr);
-        return tolower(ch);
+		struct termios oldattr, newattr;
+		short ch;
+		tcgetattr(1, &oldattr);
+		newattr = oldattr;
+		newattr.c_lflag &= ~(ICANON | ECHO);
+		tcsetattr(1, TCSANOW, &newattr);
+		ch = getchar();
+		tcsetattr(1, TCSANOW, &oldattr);
+		return tolower(ch);
 }
 #endif
 
@@ -49,37 +49,37 @@ char btt_getch(void)
 #ifdef _WIN32
 char btt_kbhit()
 {
-        return _kbhit();
+		return _kbhit();
 }
 
 #elif __unix__
 
 char btt_kbhit()
 {
-        struct termios oldt, newt;
-        char out;
-        char * ch = (char *) malloc(KBHIT_BUFFER);
-        short oldf;
+		struct termios oldt, newt;
+		char out;
+		char * ch = (char *) malloc(KBHIT_BUFFER);
+		short oldf;
 
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+		tcgetattr(STDIN_FILENO, &oldt);
+		newt = oldt;
+		newt.c_lflag &= ~(ICANON | ECHO);
+		tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+		oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+		fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-        if(!(fscanf(stdin,"%s", ch))) {
-                fprintf(stderr, "error -> %d\n", EIO);
-                exit(1);
-        }
+		if(!(fscanf(stdin,"%s", ch))) {
+				fprintf(stderr, "error -> %d\n", EIO);
+				exit(1);
+		}
 
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        fcntl(STDIN_FILENO, F_SETFL, oldf);
+		tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+		fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-        out = ch[0];
+		out = ch[0];
 
-        free(ch);
+		free(ch);
 
-        return tolower(out);
+		return tolower(out);
 }
 #endif
