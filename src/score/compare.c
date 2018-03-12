ced553a6403a4089d6c95f25aecfbc45b149d5a7
Juan Ocampos <ocamposjuanc@gmail.com>
2018-03-12 11:27:26 -0300

    qsort bug solved

--- src/score/compare.c
+++ src/score/compare.c
@@ -1,9 +1,14 @@
+#include <stdio.h>
 #include "include/score.h"
 
 int
 score_compare (const void * actual, const void * other)
 {
-		int actual_points = (int) ((score_t *) actual)->points;
-		int other_points = (int) ((score_t *) other)->points;
-		return actual_points - other_points;
+		score_t * actual_score = *((score_t **) actual);
+		score_t * other_score = *((score_t **) other);
+		int actual_points = actual_score->points;
+		int other_points = other_score->points;
+		if (actual_points > other_points) return 1;
+		if (actual_points < other_points) return -1;
+		return 0;
 }
