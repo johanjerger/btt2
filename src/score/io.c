92254c6b1c225a6da92bb9f69c3ce352b48d2320
ocamposjuancruz23 <ocamposjuancruz23@gmail.com>
2018-03-29 09:41:41 -0300

    Added error handlers

--- src/score/io.c
+++ src/score/io.c
@@ -2,18 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include "include/score.h"
+#include "../utility/include/error.h"
+#include "../utility/include/utility.h"
 
 score_t *
 score_read (FILE * score_table_file)
 {
-		int size_of_name, points, level;
+		size_t int_size = sizeof(int);
+		size_t char_size = sizeof(char);
+		int result;
+	  int size_of_name, points, level;
 		char * player_name;
 
-		fread(&size_of_name, sizeof(int), 1, score_table_file);
-		player_name = (char *) malloc(sizeof(char) * size_of_name + 5);
-		fread(player_name, sizeof(char), size_of_name + 1, score_table_file);
-		fread(&points, sizeof(int), 1, score_table_file);
-		fread(&level, sizeof(int), 1, score_table_file);
+		// Getting the lenght of the name the player logged for his score.
+		result = fread(&size_of_name, int_size, 1, score_table_file);
+		check_int_error(result, int_size, FREAD_ERROR, FREAD_ERROR_NAMESIZE_MSG);
+
+		// Getting the name the player logged for his score.
+		int to_alloc = char_size * size_of_name + 5;
+		player_name = (char *) malloc(to_alloc);
+		result = fread(player_name, char_size, size_of_name + 1, score_table_file);
+		check_int_error(result, size_of_name+1, FREAD_ERROR, FREAD_ERROR_NAME_MSG);
+
+		// Getting the score of the player.
+		result = fread(&points, int_size, 1, score_table_file);
+		check_int_error(result, int_size, FREAD_ERROR, FREAD_ERROR_SCORE_MSG);
+
+		// Getting the level when the player end his play.
+		result = fread(&level, int_size, 1, score_table_file);
+		check_int_error(result, int_size, FREAD_ERROR, FREAD_ERROR_LEVEL_MSG);
 
 		return new_score(player_name, points, level);
 }
